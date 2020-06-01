45262100b09a5025c8bed9f8e2a7a1e59f14126f
Sanjay Kumar Prasad <sprasad@XEBIAINDIA.LOCAL>
2020-06-01 23:53:39 +0530

    final  commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {createStore, applyMiddleware,compose} from  'redux';
+import { Provider } from 'react-redux';
+import reducer from './Reducers/reducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer,composeEnhancers(applyMiddleware(thunk)));
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
